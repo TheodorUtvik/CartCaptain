fcda93a1c4044e034f7b0c37860b38d3c6dfba79
SimonSnildal <147409556+SimonSnildal@users.noreply.github.com>
2024-02-26 14:02:54 +0100

    Lagt inn fooType

--- src/main/java/FoodItem.java
+++ src/main/java/FoodItem.java
@@ -115,6 +115,6 @@ public class FoodItem {
    */
 
   public void printInfo() {
-    System.out.println("Name: " + name + ", Quantity: " + quantity + ", Unit: " + unit);
+    System.out.println("Name: " + name + ", Quantity: " + quantity + ", Unit: " + unit + ", Type: " + foodType
   }
 }
