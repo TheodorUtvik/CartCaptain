1e12a57d7749f4ae724f6a197b4d07f0826a974c
Theodor <theodor.utvik@gmail.com>
2024-03-15 13:16:11 +0100

    Added methods in shoppingListController to search and remove search and initialize.

--- src/main/java/controllers/ShoppingListController.java
+++ src/main/java/controllers/ShoppingListController.java
@@ -2,17 +2,23 @@ package controllers;
 
 import static controllers.SceneUtils.changeScene;
 
-import java.awt.TextField;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import entities.FoodItem;
+import file_handling.FileHandler;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.TextField;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javax.swing.text.html.ListView;
+import javafx.scene.control.ListView;
+import userinterfacetools.UserInterfaceMethods;
+import javafx.fxml.Initializable;
 
-public class ShoppingListController  {
 
-  HashMap<String, ArrayList<String>> shoppingList = new HashMap<>();
+public class ShoppingListController implements Initializable {
+
 
   @FXML
   private void goBack(ActionEvent event) {
@@ -25,10 +31,41 @@ public class ShoppingListController  {
   @FXML
   private ListView<String> shoppingListView;
 
-  @FXML
-  void searchBar(ActionEvent event) {
-    shoppingListView.getItems().clear();
-    shoppingListView.getItems().addAll(shoppingList.get(searchBar.getText()));
+  /**
+   * Initializes the controller class. This method is automatically called after the fxml file has
+   * been loaded.
+   */
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    searchBar.textProperty().addListener((observable, oldValue, newValue) -> {
+      searchBarTextChanged(); // This will be called every time the text in searchBar changes
+    });
+    // Any other initialization code can go here
   }
+
+  /**
+   * This method should be triggered by a listener on the TextField's textProperty to update the
+   * ListView with the search results. It should be called every time the text in the TextField
+   * changes.
+   */
+  @FXML
+  private void onSearchButtonClicked() {
+    shoppingListView.getItems().clear();
+    searchBarTextChanged();
+  }
+  private void searchBarTextChanged() { // This method should be triggered by a listener on the TextField's textProperty
+    String searchQuery = searchBar.getText().toLowerCase();
+    Iterator<FoodItem> allFoodItems = FileHandler.readFoodFromFile(
+        "src/main/resources/foodItems.csv"); // Adjust the path as needed
+    while (allFoodItems.hasNext()) {
+      FoodItem foodItem = allFoodItems.next();
+      if (foodItem.getName().toLowerCase().contains(searchQuery)) {
+        shoppingListView.getItems().add(foodItem.getName());
+      }
+    }
+  }
+
+
+
 
 }
