1668f929fb7d9f1809f1fdb731bc865499b08927
Theodor <theodor.utvik@gmail.com>
2024-03-15 15:01:17 +0100

    Added functionality to add and remove food item from shooping list

--- src/main/java/controllers/ShoppingListController.java
+++ src/main/java/controllers/ShoppingListController.java
@@ -6,6 +6,7 @@ import entities.FoodItem;
 import file_handling.FileHandler;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import java.util.Iterator;
 import java.util.List;
@@ -21,12 +22,21 @@ public class ShoppingListController implements Initializable {
 
 
   @FXML
+  public Button addButton;
+
+  @FXML
+  public Button removeButton;
+
+  @FXML
   private void goBack(ActionEvent event) {
     changeScene(event, "/scenebuilderjavafxapp/CartCaptainFrontPage.fxml", "Front Page");
   }
 
   @FXML
   private TextField searchBar;
+
+  @FXML
+  private ListView<String> foodItemsView;
 
   @FXML
   private ListView<String> shoppingListView;
@@ -41,6 +51,14 @@ public class ShoppingListController implements Initializable {
       searchBarTextChanged(); // This will be called every time the text in searchBar changes
     });
     // Any other initialization code can go here
+    foodItemsView.setOnMouseClicked(event -> {
+      if (event.getClickCount() == 2) {
+        String selectedItem = foodItemsView.getSelectionModel().getSelectedItem();
+        if (selectedItem != null && !shoppingListView.getItems().contains(selectedItem)) {
+          shoppingListView.getItems().add(selectedItem);
+        }
+      }
+    });
   }
 
   /**
@@ -50,22 +68,47 @@ public class ShoppingListController implements Initializable {
    */
   @FXML
   private void onSearchButtonClicked() {
-    shoppingListView.getItems().clear();
+    foodItemsView.getItems().clear();
     searchBarTextChanged();
   }
   private void searchBarTextChanged() { // This method should be triggered by a listener on the TextField's textProperty
+    foodItemsView.getItems().clear();
     String searchQuery = searchBar.getText().toLowerCase();
     Iterator<FoodItem> allFoodItems = FileHandler.readFoodFromFile(
         "src/main/resources/foodItems.csv"); // Adjust the path as needed
     while (allFoodItems.hasNext()) {
       FoodItem foodItem = allFoodItems.next();
-      if (foodItem.getName().toLowerCase().contains(searchQuery)) {
-        shoppingListView.getItems().add(foodItem.getName());
+      if (foodItem.getName().toLowerCase().contains(searchQuery)){
+        foodItemsView.getItems().add(foodItem.getName());
       }
     }
   }
 
+  /**
+   * This method will allow the user to add items to the shopping list. It will be triggered by a
+   * button click. It should add the selected item to the shopping list, which is a new list of
+   * strings. The shopping list should be displayed in a ListView. The user should be able to add
+   * multiple items to the shopping list.
+   */
+  @FXML
+  private void onAddToShoppingListButtonClicked() {
+    String selectedItem = foodItemsView.getSelectionModel().getSelectedItem();
+    if (selectedItem != null && !shoppingListView.getItems().contains(selectedItem)) {
+      shoppingListView.getItems().add(selectedItem);
+    }
+  }
 
+  /**
+   * This method will allow the user to remove items from the shopping list. It will be triggered by a
+   * button click. It should remove the selected item from the shopping list.
+   */
+  @FXML
+  private void onRemoveFromShoppingListButtonClicked() {
+    String selectedItem = shoppingListView.getSelectionModel().getSelectedItem();
+    if (selectedItem != null) {
+      shoppingListView.getItems().remove(selectedItem);
+    }
+  }
 
 
 }
